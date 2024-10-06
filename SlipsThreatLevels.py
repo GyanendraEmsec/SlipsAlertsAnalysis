37f8536fd3bb48a895b872ebf70e6065aa7ff374
GyanendraEmsec <gyanendra@emsec.uk>
2024-10-06 22:08:44 +0530

    Graph for Threat levels and alerts counts

--- SlipsThreatLevels.py
+++ SlipsThreatLevels.py
@@ -1,5 +1,6 @@
 import elasticsearch
 from elasticsearch import Elasticsearch
+import matplotlib.pyplot as plt
 
 es5 = Elasticsearch(
     cloud_id="xxxxxxxxxxxxxxx",
@@ -46,6 +47,25 @@ query1 = {
 result = es5.search(index=index_name, body=query1)
 
 threat_levels = result['aggregations']['threat_levels']['buckets']
-
+threatLevels = []
+counts = []
 for bucket in threat_levels:
     print(f"Threat_level: {bucket['key']}, Count: {bucket['doc_count']}")
+    threatLevels.append(bucket['key'])
+    counts.append(bucket['doc_count'])
+
+plt.figure(figsize=(13,10))
+bars = plt.bar(threatLevels, counts, color=['blue', 'green', 'orange', 'red'])
+
+# Adding counts on top of bars
+for bar in bars:
+    yval = bar.get_height()
+    plt.text(bar.get_x() + bar.get_width()/2, yval + 500, int(yval), ha='center', va='bottom')
+
+# Title and labels
+plt.title('Distribution of All Slips Alerts Over Threat Levels')
+plt.xlabel('Threat Levels')
+plt.ylabel('Alerts Count')
+
+# Display the plot
+plt.show()
